c95904cd3094cf4b739634896f93ed412b6e76c6
riuzaki16 <riuzakii16@gmail.com>
2025-03-25 15:33:11 +0300

    weather programm

--- main.go
+++ main.go
@@ -5,19 +5,22 @@ import (
 	"fmt"
 
 	"github.com/riuzaki24/app-5/geo"
+	"github.com/riuzaki24/app-5/weather"
 )
 
 func main() {
-	// fmt.Println("Новый проект")
+	fmt.Println("Новый проект")
 
 	city := flag.String("city", "", "Город пользователя")
-	// format := flag.Int("format", 1, "Формат вывода погода")
+	format := flag.Int("format", 1, "Формат вывода погода")
 
 	flag.Parse()
+	
 	fmt.Println(*city)
 	geoData, err := geo.GetMyLocation(*city)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	fmt.Println(geoData)
+	weatherData := weather.GetWeather(*geoData, *format)
+	fmt.Println(weatherData)
 }
